843193f0d8fc3316b5d8296164fd4f15d00c4324
Matto58 <thematto58@gmail.com>
2025-07-28 17:20:38 +0200

    create cartridge.c

---
+++ src/hypervisor/cartridge.c
@@ -0,0 +1,5 @@
+#include "hypervisor.h"
+
+void *crGetMainProgram(hypervisor_t *parentHv, uint64_t *readSize, uint64_t *bufSize) {
+    return NULL; // todo: implement after moving hvReadFile & hvWriteFile to mmfsutils.c
+}
--- src/hypervisor/hypervisor.h
+++ src/hypervisor/hypervisor.h
@@ -8,13 +8,22 @@
 
 #pragma once
 
-// todo: add more shit, this is p barebones for now
-typedef struct {
+struct __cartridge_t {
     FILE *drive;
-} hypervisor_t;
+    char *name;
+};
+typedef struct __cartridge_t cartridge_t;
+// todo: add more shit, this is p barebones for now
+struct __hypervisor_t {
+    FILE *drive;
+    cartridge_t *mountedCart;
+};
+typedef struct __hypervisor_t hypervisor_t;
 
 void *hvReadFile(hypervisor_t *hvCtx, int16_t partInx, char *fileName, uint64_t *readSize, uint64_t *bufSize);
 bool hvWriteFile(hypervisor_t *hvCtx, int16_t partInx, char *fileName, void *data, uint64_t size);
 hypervisor_t *hvInit(char *driveLocation);
 void hvDestroy(hypervisor_t *hvCtx);
 bool hvIsValid(hypervisor_t *hvCtx);
+
+void *crGetMainProgram(hypervisor_t *parentHv, uint64_t *readSize, uint64_t *bufSize);
